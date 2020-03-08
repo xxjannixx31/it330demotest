e6c0c61457795639bdcda1876c6a119a499c0804
xxjannixx31 <61930778+xxjannixx31@users.noreply.github.com>
2020-03-08 22:10:54 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,27 @@
-<?php
+ <?php
+$servername = "it330serve130.mysql.database.azure.com";
+$username = "johnryan@it330serve130";
+$password = "Animalka123";
+$dbname = "itazure";
 
-	echo "hello world"
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-?>
+$sql = "SELECT id, description, count FROM inventory";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "ID: " . $row["id"]. " - Desciption: " . $row["firstname"]. " Count" . $row["lastname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?> 
